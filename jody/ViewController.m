323b7ad664d6fdb5383babd7208cfeba78ea70ef
zsweedyk <z@cs.hmc.edu>
2015-07-07 14:01:21 -0700

    Initial Commit

---
+++ jody/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  jody
+//
+//  Created by Z Sweedyk on 7/7/15.
+//  Copyright (c) 2015 Z Sweedyk. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
