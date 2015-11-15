bedc1a8b7efd464c8e98d53e08604269319044f1
zsweedyk <z@cs.hmc.edu>
2015-11-15 14:42:39 -0800

    misc. fixes

--- jody/constants.m
+++ jody/constants.m
@@ -16,8 +16,8 @@ int const kPDFHeight = 792;
 float const basicColors[9][3]={
     {.75,.9,.9},
     {.65,.50,.39},
-    {.65,.75,.60},
     {.66,.65,.64},
+    {.65,.75,.60},
     {.7,.75,.9},
     {.75,.7,.8},
     {.83,.83,.83},
@@ -29,8 +29,8 @@ int const kHeadlineFontSize[7]={52,56,56,72,72,96,52};
 int const kChainFontSize[7] = {20,20,20,28,28,36,20};
 int const kStartScreenLargeFontSize[7]={52,56,56,72,72,96,52};
 int const kStartScreenSmallFontSize[7]={32,32,32,48,48,56,32};
-int const kInfoScreenFontSize[7]={10,10,12,14,18,18,10};
-int const kToolBarFontSize[7]={10,10,10,14,18,18,10};
+int const kInfoScreenFontSize[7]={12,13,13,14,18,18,10};
+int const kToolBarFontSize[7]={12,13,13,14,18,18,10};
 
 
     
